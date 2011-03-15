a246395cdca8266cdbf8c30c1d6b70547535fe30
David Padbury <david@davidpadbury.com>
2011-03-14 21:06:47 -0400

    removing bits from spec to fill in

--- StuffToDo-7-Jasmine/StuffToDo.Web/Scripts/tests/spec/lab.todoItemSpec.js
+++ StuffToDo-7-Jasmine/StuffToDo.Web/Scripts/tests/spec/lab.todoItemSpec.js
@@ -11,33 +11,4 @@ describe('lab.todoItem', function () {
         });
     });
 
-    describe('when clicking delete', function () {
-
-        beforeEach(function () {
-            spyOn($, 'ajax');
-
-            todoItem.find('a.delete').click();
-        });
-
-        it('should make delete call', function () {
-
-            expect($.ajax).toHaveBeenCalled();
-
-        });
-
-        describe('when delete completes', function () {
-
-            beforeEach(function () {
-                var ajaxArgs = $.ajax.mostRecentCall.args[0];
-                ajaxArgs.complete();
-            });
-
-            it('should remove item', function () {
-                expect( $('#todo-item-fixture').children().length ).toEqual(0);
-            });
-
-        });
-
-    });
-
 });
